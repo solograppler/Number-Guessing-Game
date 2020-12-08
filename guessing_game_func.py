3b36ecfa77a1ce9baa7ff64a7b809466d30c0613
solograppler <joloves@gmail.com>
2020-12-08 09:32:10 -0500

    Create guessing_game_func.py

---
+++ guessing_game_func.py
@@ -0,0 +1,43 @@
+import random
+
+random_num = random.randint(0, 10)
+it_took = 0
+number_guess = 5
+
+
+def inputNumber(message):
+    while True:
+        try:
+            userInput = int(input(message))
+        except ValueError:
+            print("This is not a number please try again.")
+            continue
+        else:
+            return userInput
+            break
+
+
+print(random_num)
+print("Welcome to the guessing game, you will have 5 guesses to guess a number.")
+guess = inputNumber("Please enter a number from 0 and 10: ")
+
+while number_guess > 1:
+
+    if guess > random_num:
+        it_took += 1
+        number_guess -= 1
+        guess = int(input("Too high, try again: "))
+    elif guess < random_num:
+        it_took += 1
+        number_guess -= 1
+        guess = int(input("Too Low, try again: "))
+    elif guess == random_num:
+        it_took += 1
+        number_guess -= 1
+        break
+
+if number_guess <= 1:
+    print("I am sorry, you have run out of guesses.")
+else:
+    print("Winner, Winner, Chicken Dinner")
+    print("Congratulations it took you %s guesses to guess the magic number." % it_took)
